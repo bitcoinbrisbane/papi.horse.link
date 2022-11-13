240c0469a8d2624a16826b20422d7910f16bfd4a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-11-13 10:19:37 +1000

    feat: change port, add registry contracts

--- src/index.py
+++ src/index.py
@@ -1,8 +1,57 @@
 #!/usr/bin/env python3
 from flask import Flask
-from web3 import Web3
+from web3 import w3
 
 app = Flask(__name__)
+
+
+def getMarketAddresses():
+    registry_address = "0x885386d140e4321102dc218060Bbd55a8B020F4C"
+    registry_abi = [
+        {
+            "inputs": [],
+            "name": "marketCount",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "markets",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+
+    registry = w3.eth.contract(address=registry_address, abi=registry_abi)
+    count = registry.functions.marketCount().call()
+
+    addresses = []
+
+    for i in range(count):
+        addresses.append(registry.functions.markets(i).call())
+
+
+    return addresses
 
 
 @app.route('/markets/', methods=['GET'])
@@ -11,5 +60,4 @@ def welcome():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
-
+    app.run(host='127.0.0.1', port=3000)
